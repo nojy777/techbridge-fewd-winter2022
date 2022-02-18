d54597cb1189b7583a5ea769c7583378901e0d1a
enyawevia <nojy777@gmail.com>
2022-02-18 16:30:02 -0600

    updated script.js file and added Book.html file to project phase2

--- Project-phase2/scripts.js
+++ Project-phase2/scripts.js
@@ -35,6 +35,21 @@ function reloadPage() {
 
 function findInGrid() {
  //find in grid
+ let indexFound = -1;
+    let numberToFind = document.getElementById("tbNumberToFind").value;
+    let spans = document.getElementsByTagName('span');
+    for (let index = 0; index < spans.length; index++) {
+        if(numberToFind === spans[index].outerText){
+            indexFound = index;
+            break;
+        }
+    }
+    if(indexFound >= 0) {
+        spans[indexFound].classList.add('spanHighlighted');
+    } else {
+        alert(numberToFind + " is not in the Grid");
+    }
+
 }
 
 function findInList() {
@@ -42,7 +57,7 @@ function findInList() {
    const textToFind = document.getElementById("tbTextToFind").value;
    console.log(textToFind);
 
-   let indexFound;
+   let indexFound = -1;
    let spans = document.getElementsByTagName('span');
    for (let index = 0; index < spans.length; index++) {
         if(textToFind === spans[index].outerText){
@@ -50,10 +65,30 @@ function findInList() {
             break;
         }
     }
-   
-    spans[indexFound].classList.add("spanHighlighted");
+    if(indexFound >=0){
+        spans[indexFound].classList.add("spanHighlighted");
+
+    }else{
+        alert(textToFind + " is not in the list");
+    }
+    
 }
 
 function sortIt() {
-    //
+    let spans = document.getElementsByTagName('span');
+    for (let index = 0; index < spans.length; index++) {
+        spans[index].outerText = '';
+    }
+    let temp;
+    for (let index = 0; index < spans.length; index++) {
+        for (let x = index + 1; x < spans.length-1; x++) {
+            if(spans[index].outerText < spans[x].outerText) {
+                console.log(spans[index].outerText)
+                temp = spans[index].outerText;
+                spans[index].outerText = spans[x].outerText;
+                spans[x].outerText = temp;
+                break;
+            }
+        }
+    }
 }
